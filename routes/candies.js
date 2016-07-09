3f95e62d9f5aa14945b246f4def50385568506d8
a.kanta22@gmail.com <a.kanta22@gmail.com>
2016-07-09 17:50:18 +0800

    Candies iteration. ready for CI

---
+++ routes/candies.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ res.render('index', { title: 'Express' });
+});
+
+var object = [
+ {"id":1,"name":"Chewing Gum", "color":"Red"},
+ {"id":2,"name":"Pez","color":"Green"},
+ {"id":3,"name":"Marshmallow","color":"Pink"},
+ {"id":4,"name":"Candy Stick","color":"Blue"}
+];
+
+// INDEX
+router.get('/candies', (req, res) => {
+ res.status(200).json(object)
+})
+
+// SHOW
+router.get('/candies/:id', (req, res) => {
+ res.status(200).json(object[req.params.id-1]);
+});
+
+// CREATE
+router.post('/candies', (req, res, next) => {
+   console.log(req.body);
+   if(req.body.color === "wrong") {
+     res.status(422).json({
+       message: 'wrong color'
+     })
+   }
+   res.status(201).json({
+     message: 'candies created',
+     id: parseInt(req.body.id),
+     name: req.body.name,
+     color: req.body.color
+   });
+   var candy = {
+     id: parseInt(req.body.id),
+     name: req.body.name,
+     color: req.body.color
+   }
+   object.push(candy)
+});
+
+// UPDATE
+router.put('/candies/:id', (req, res) => {
+ res.status(200).json({
+   message: `Candy ${req.params.id} updated`,
+   id: req.body.id,
+   name: req.body.name,
+   color: req.body.color});
+   object[req.body.id-1].id = req.body.id
+   object[req.body.id-1].name = req.body.name
+   object[req.body.id-1].color = req.body.color
+});
+// DESTROY
+router.delete('/candies/:id', (req, res) => {
+ res.status(200).json({message: `Candy ${req.params.id} deleted`});
+ object.splice(req.params.id-1, 1)
+});
+
+module.exports = router;
---
+++ test/candies_test.js
@@ -0,0 +1,194 @@
+/* globals before describe it*/
+
+const expect = require('chai').expect
+const supertest = require('supertest')
+const api = supertest('http://localhost:3000') // you have to use server port 3000
+
+// GET candies
+describe('GET /candies', () => {
+  // done is a function
+  it('should return a 200 response', (done) => {
+    api.get('/candies')
+    .set('Accept', 'application/json')
+    .expect(200, done)
+  })
+  it('should return an array', (done) => {
+    api.get('/candies')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body.length).to.equal(response.body.length)
+      done()
+    })
+  })
+  it('should return all the records in the database', (done) => {
+    api.get('/candies')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body).to.be.an('array')
+      done()
+    })
+  })
+})
+
+// GET candies/:id
+describe('GET /candies/:id', () => {
+  // done is a function
+  it('should return a 200 response', (done) => {
+    api.get('/candies/:id')
+    .set('Accept', 'application/json')
+    .expect(200, done)
+  })
+
+  it('should return an object containing the fields "name" and "color"', (done) => {
+    api.get('/candies/3')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body).to.have.property('name')
+      expect(response.body).to.have.property('color')
+      done()
+    })
+  })
+})
+
+// POST /candies
+describe('POST /candies', () => {
+  // done is a function
+  it('should return a 200 response', (done) => {
+    api.get('/candies')
+    .set('Accept', 'application/json')
+    .expect(200, done)
+  })
+
+  it('should return a 422 response', (done) => {
+    api.post('/candies')
+    .set('Accept', 'application/json')
+    .send({
+      'id': 6,
+      'name': 'Pocky',
+      'color': 'wrong'
+    })
+    .expect(422, done)
+  })
+
+  it('should return an error message if the color field is wrong', (done) => {
+    api.post('/candies')
+      .set('Accept', 'application/json')
+      .send({
+        'id': 7,
+        'name': 'Chicken',
+        'color': 'wrong'
+      })
+      .end((error, response) => {
+        expect(error).to.be.a('null')
+        expect(response.body.message).to.equal('wrong color')
+        done()
+      })
+  })
+
+  before((done) => {
+    api.post('/candies')
+      .set('Accept', 'application/json')
+      .send({
+        'id': 5,
+        'name': 'lollipop',
+        'color': 'red'
+      }).end(done)
+  })
+  it('should add a candy object to the collection and return it', (done) => {
+    api.get('/candies')
+      .set('Accept', 'application/json')
+      .end((error, response) => {
+        expect(error).to.be.a('null')
+        // expect (response.body.length).to.equal(5)
+        expect(response.body[response.body.length - 1].name).to.equal('lollipop')
+        done()
+      })
+  })
+
+  it('should return an error if the color is wrong', (done) => {
+    api.post('/candies')
+      .set('Accept', 'application/json')
+      .send({
+        'id': 8,
+        'name': 'Duck',
+        'color': 'wrong'
+      })
+      .end((error, response) => {
+        expect(error).to.be.a('null')
+        expect(response.body.message).to.equal('wrong color')
+        done()
+      })
+  })
+})
+
+// DELETE
+describe('DELETE /candies/:id', () => {
+  // done is a function
+  before((done) => {
+    api.delete('/candies/3')
+      .set('Accept', 'application/json')
+      .end(done)
+  })
+  it('should remove a candy document', (done) => [
+    api.get('/candies/3')
+      .set('Accept', 'application/json')
+      .end((error, response) => {
+        expect(error).to.be.a('null')
+        console.log(response.body.color)
+        expect(response.body.color).to.equal('Blue')
+        done()
+      })
+  ])
+})
+// PUT
+describe('PUT /candies/:id', () => {
+  // done is a function
+  it('should return a 200 response', (done) => {
+    api.put('/candies/2')
+    .set('Accept', 'application/json')
+    .expect(200, done)
+  })
+  before((done) => {
+    api.put('/candies/2')
+      .set('Accept', 'application/json')
+      .send({
+        'id': 2,
+        'name': 'Winston',
+        'color': 'Black'
+      }).end(done)
+  })
+
+  it('should update a candy document', (done) => {
+    api.get('/candies/2')
+      .set('Accept', 'application/json')
+      .end((error, response) => {
+        expect(error).to.be.a('null')
+        console.log(response.body)
+        // expect (response.body.length).to.equal(5)
+        expect(response.body.name).to.equal('Winston')
+        done()
+      })
+  })
+})
+
+  // it('should return an array', (done) => {
+  //   api.get('/candies')
+  //   .set('Accept', 'application/json')
+  //   .end((error, response) => {
+  //     expect(error).to.be.a('null')
+  //     expect(response.body).to.be.an('array')
+  //     done()
+  //   })
+  // })
+  // it('should return an object that has a field called 'name"', (done) => {
+  //   api.get('/candies')
+  //   .set('Accept', 'application/json')
+  //   .end((error, response) => {
+  //     expect(error).to.be.a('null')
+  //     expect(response.body[0]).to.be.property('name')
+  //     done()
+  //   })
+  // })
